c4f14d1bb92fc0200f23af174861686e9d8eae58
KhoiNguyenDev <khoindt10a4@gmail.com>
2025-07-09 14:48:52 +0700

    create theater model

---
+++ src/main/java/Model/Theater.java
@@ -0,0 +1,88 @@
+package Model;
+
+public class Theater {
+	private int id;
+	private String name;
+	private String status;
+	private int capacity;
+	private int seatCount;
+	private String description;
+	private String image;
+
+	protected Theater() {
+	}
+
+	protected Theater(int id, String name, String status, int capacity, int seatCount, String description,
+			String image) {
+		this.id = id;
+		this.name = name;
+		this.status = status;
+		this.capacity = capacity;
+		this.seatCount = seatCount;
+		this.description = description;
+		this.image = image;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int getSeatCount() {
+		return seatCount;
+	}
+
+	public void setSeatCount(int seatCount) {
+		this.seatCount = seatCount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "Theater [id=" + id + ", name=" + name + ", status=" + status + ", capacity=" + capacity + ", seatCount="
+				+ seatCount + ", description=" + description + ", image=" + image + "]";
+	}
+
+}
