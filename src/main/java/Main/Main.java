f4fdf0c63048163a267f0119dd5b8e0545b5e8cd
KhoiNguyenDev <khoindt10a4@gmail.com>
2025-07-11 00:11:52 +0700

    bm

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,18 +1,14 @@
 package Main;
 
-import java.time.LocalDateTime;
-
 import javax.swing.SwingUtilities;
 
-import Enum.AccountEnum;
-import Model.Account;
-import View.HomeUseView;
+import View.Admin.AdminDashboard;
 
 public class Main {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> new HomeUseView(new Account(1, "1", "1", AccountEnum.Role.CUSTOMER,
-				AccountEnum.Status.ACTIVE, "k", "sssss", LocalDateTime.now(), LocalDateTime.now())).setVisible(true));
+//		SwingUtilities.invokeLater(() -> new HomeUseView(new Account(1, "1", "1", AccountEnum.Role.CUSTOMER,
+//				AccountEnum.Status.ACTIVE, "k", "sssss", LocalDateTime.now(), LocalDateTime.now())).setVisible(true));
 //		SwingUtilities.invokeLater(() -> new LoginView().setVisible(true));
-//		SwingUtilities.invokeLater(() -> new AdminDashboard().setVisible(true));
+		SwingUtilities.invokeLater(() -> new AdminDashboard().setVisible(true));
 	}
 }
